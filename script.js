12bc7d24833c89c8cc958bac80b4ab1c4c242e4a
Kevin Andel Josy <83870616+kevinandel@users.noreply.github.com>
2024-09-27 22:51:59 +0530

    Small update in scrpt.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Create a "close" button and append it to each list item
+// Create a "close" button and append it to each of list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
